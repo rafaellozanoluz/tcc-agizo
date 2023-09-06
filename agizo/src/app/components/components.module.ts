fd0eaf54cd37d978d906254a8ae420ca59eb4254
Rafael Lozano da Luz <rafaellozanopro@outlook.com>
2023-09-05 22:44:18 -0300

    Atualizando criação de curriculo

--- agizo/src/app/components/components.module.ts
+++ agizo/src/app/components/components.module.ts
@@ -7,12 +7,11 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarDefaultComponent } from './navbar-default/navbar-default.component';
 import { CurriculoAtuacaoComponent } from './curriculo-atuacao/curriculo-atuacao.component';
-import { CurriculoCabecalhoComponent } from './curriculo-cabecalho/curriculo-cabecalho.component';
 import { CurriculoResumoComponent } from './curriculo-resumo/curriculo-resumo.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, NgbModule],
-  declarations: [FooterComponent, NavbarComponent, SidebarComponent, NavbarDefaultComponent, CurriculoAtuacaoComponent, CurriculoCabecalhoComponent, CurriculoResumoComponent],
-  exports: [FooterComponent, NavbarComponent, SidebarComponent, NavbarDefaultComponent, CurriculoAtuacaoComponent, CurriculoCabecalhoComponent, CurriculoResumoComponent], 
+  declarations: [FooterComponent, NavbarComponent, SidebarComponent, NavbarDefaultComponent, CurriculoAtuacaoComponent, CurriculoResumoComponent],
+  exports: [FooterComponent, NavbarComponent, SidebarComponent, NavbarDefaultComponent, CurriculoAtuacaoComponent, CurriculoResumoComponent], 
 })
 export class ComponentsModule {}
